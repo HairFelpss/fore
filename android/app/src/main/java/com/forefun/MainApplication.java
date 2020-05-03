dd24553d90f40b29ed7d7fb94d149ae4848dfed2
felipe <fajreyy.bh@gmail.com>
2020-05-03 14:25:59 -0300

    fix bugs

--- android/app/src/main/java/com/forefun/MainApplication.java
+++ android/app/src/main/java/com/forefun/MainApplication.java
@@ -29,8 +29,8 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          packages.add(new CodePush(BuildConfig.CODEPUSH_deploymentKey, MainApplication.this, BuildConfig.DEBUG));
           packages.add(new LinearGradientPackage());
+          packages.add(new CodePush(BuildConfig.CODEPUSH_deploymentKey, MainApplication.this, BuildConfig.DEBUG));
           return packages;
         }
         
